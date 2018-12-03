817d1d47a8a5ed1e3df02ffea95de877d9fe4384
Trent Millar <trent.millar@gmail.com>
2018-12-03 11:49:25 -0700

    rm unused

--- samples/Mvc.Sample/Program.cs
+++ samples/Mvc.Sample/Program.cs
@@ -10,15 +10,19 @@ using Microsoft.Extensions.Logging;
 
 namespace Mvc.Sample
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.Add()
+                })
                 .UseStartup<Startup>();
     }
 }
--- src/Keystone.Abstractions/KeystoneBuilder.cs
+++ src/Keystone.Abstractions/KeystoneBuilder.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel;
 using JetBrains.Annotations;
+// ReSharper disable All
 
 namespace Microsoft.Extensions.DependencyInjection
 {
